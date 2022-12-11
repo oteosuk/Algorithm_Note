4472fd2d71d94cdd41f87b5ef232e3997e1bae7a
oteosuk <oteosuk@naver.com>
2022-12-12 04:36:49 +0900

    제출용 소스파일 생성

---
+++ Test/제출용.cpp
@@ -0,0 +1,14 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+#define endl '\n'
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    return 0;
+}
