6344a31e0dd64b0b2b782079f67963b424efc5b2
oteosuk <oteosuk@naver.com>
2022-12-22 01:01:39 +0900

    커밋

--- Test/제출용.cpp
+++ Test/제출용.cpp
@@ -6,22 +6,5 @@ using namespace std;
 
 int main() {
     
-    int n;
-    cin >> n;
-
-    while (n--) {
-        int a;
-        cin >> a;
-        while (a--) {
-            int x;
-            cin >> x;
-            
-        }
-
-    }
-
-
-
-    
     return 0;
 }
