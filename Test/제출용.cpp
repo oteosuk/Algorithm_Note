64ad4ce1edc86744a8d82459a2df69c55174b7a3
oteosuk <oteosuk@naver.com>
2022-12-21 16:55:49 +0900

    12/21 커밋

--- Test/제출용.cpp
+++ Test/제출용.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
 #define endl '\n'
 
+priority_queue<int, vector<int>, greater<int>> pq;
+
 int main() {
-    
-    
+
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int N; cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        int temp; cin >> temp;
+
+        if (temp == 0) {
+            if (pq.empty()) {
+                cout << 0 << endl;
+            }
+            else {
+                cout << pq.top() << endl;
+                pq.pop();
+            }
+        }
+        else {
+            pq.push(temp);
+        }
+    }
 
     return 0;
 }
