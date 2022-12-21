f01af0ab73faeaa2d7f983e5bd1a1a279d1dfb67
oteosuk <oteosuk@naver.com>
2022-12-22 01:02:06 +0900

    12/22 커밋

--- Test/제출용.cpp
+++ Test/제출용.cpp
@@ -4,7 +4,15 @@ using namespace std;
 
 #define endl '\n'
 
+
+
 int main() {
-    
+
+
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int N; cin >> N;
+
+
     return 0;
 }
