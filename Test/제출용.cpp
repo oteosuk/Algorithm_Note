5ada4856a74d2a4a174030caddaf366f8d561ba2
oteosuk <oteosuk@naver.com>
2022-12-24 12:15:52 +0900

    Update 제출용.cpp

--- Test/제출용.cpp
+++ Test/제출용.cpp
@@ -34,6 +34,13 @@ void Z(int y, int x, int size)
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+<<<<<<< Updated upstream
+=======
+
+    int N; cin >> N;
+
+
+>>>>>>> Stashed changes
 
     cin >> N >> r >> c;
     
