1809c83649e31367bafbdf48ad4cb70058b333cf
oteosuk <oteosuk@naver.com>
2023-01-02 01:40:38 +0900

    1/2 커밋

---
+++ BOJ_Practice/BOJ 1541.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define endl '\n'
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	string s;
+	cin >> s;
+
+	int ans = 0;
+	string num;
+	bool minus = false;
+
+	for (int i = 0; i <= s.size(); i++) {
+		if (i == s.size() || s[i] == '-' || s[i] == '+') {
+			if (minus) {
+				ans -= stoi(num);
+				num = "";
+			}
+			else {
+				ans += stoi(num);
+				num = "";
+			}
+		}
+		else
+			num += s[i];
+
+		if (s[i] == '-') {
+			minus = true;
+		}
+	}
+
+	cout << ans;
+
+	return 0;
+}
