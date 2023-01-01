c6facc522c3eb5f474366c086241bcd1bfcadf57
oteosuk <oteosuk@naver.com>
2023-01-02 01:41:04 +0900

    1/2 커밋

--- Test/제출용.cpp
+++ Test/제출용.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
@@ -7,31 +6,6 @@ using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
-	string s;
-	cin >> s;
-
-	int ans = 0;
-	string num;
-	bool minus = false;
-
-	for (int i = 0; i <= s.size(); i++) {
-		if (i == s.size() || s[i] == '-' || s[i] == '+') {
-			if (minus) {
-				ans -= stoi(num);
-				num = "";
-				minus = true;
-			}
-			else {
-				ans += stoi(num);
-				num = "";
-			}
-		}
-		else 
-			num += s[i];
-	}
-
-	cout << ans;
 
 	return 0;
 }
