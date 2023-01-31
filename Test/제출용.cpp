b357cb7f82b80acec8dad78defddbf714b8d3756
oteosuk <oteosuk@naver.com>
2023-01-31 15:39:43 +0900

    1/31 커밋

--- Test/제출용.cpp
+++ Test/제출용.cpp
@@ -1,38 +1,14 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
 #define endl '\n'
 
-bool compare(const pair<int, int>& a, const pair<int, int>& b) {
-	return a.second < b.second;
-}
-
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-	int N; cin >> N;
+	int M, N; cin >> M >> N;
 
-	vector<pair<int, int>> p(N);
-
-	for (int i = 0; i < N; i++) {
-		cin >> p[i].first >> p[i].second;
-	}
-
-	sort(p.begin(), p.end(), compare);
-
-	int temp = 0, cnt = 0;
-
-	for (int i = 0; i < N; i++) {
-		if (temp <= p[i].first) {
-			temp = p[i].second;
-			cnt++;
-		}
-	}
 	
-	cout << cnt;
-
 	return 0;
 }
