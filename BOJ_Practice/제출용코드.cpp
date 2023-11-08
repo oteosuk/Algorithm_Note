1ead283f04df2183e903a7ac687c8f04170da904
oteosuk <oteosuk@naver.com>
2023-11-08 10:12:47 +0900

    갱신

--- BOJ_Practice/제출용코드.cpp
+++ BOJ_Practice/제출용코드.cpp
@@ -1,7 +1,4 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
 
 using namespace std;
 
@@ -10,28 +7,6 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-    double n;
-    cin >> n;
-    if (n == 0) { cout << 0; return 0; }
-    double ex = floor(n * 0.15 + 0.5);
-    double answer = 0;
-
-    vector<int> v;
-
-    for (int i = 0; i < n; i++) {
-        int x;
-        cin >> x;
-        v.push_back(x);
-    }
-
-    sort(v.begin(), v.end());
-
-    for (int i = ex; i < n - ex; i++) {
-        answer += v[i];
-    }
-    answer = floor((answer / (n - 2 * ex)) + 0.5);
-
-    cout << answer;
-
+    
     return 0;
 }
